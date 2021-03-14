20d61b4e4a774d472bdafdceef1ce948814383a8
rnjswngus275 <rnjswngus275@gmail.com>
2021-03-15 02:47:05 +0900

    #11 1+2+3+4

--- app/src/main/java/com/example/stop_and_flight/select_signup_method.java
+++ app/src/main/java/com/example/stop_and_flight/select_signup_method.java
@@ -2,7 +2,10 @@ package com.example.stop_and_flight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
 
@@ -10,6 +13,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class select_signup_method extends AppCompatActivity {
+
+
+
     // Choose authentication providers
     List<AuthUI.IdpConfig> providers = Arrays.asList(
             new AuthUI.IdpConfig.EmailBuilder().build(),
@@ -22,5 +28,16 @@ public class select_signup_method extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_signup_method);
+        Button email_signup=(Button)findViewById(R.id.button_email);
+        email_signup.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent=new Intent(getApplicationContext(),SignupActivity.class);
+                startActivity(intent);
+
+
+            }
+
+        });
     }
 }
