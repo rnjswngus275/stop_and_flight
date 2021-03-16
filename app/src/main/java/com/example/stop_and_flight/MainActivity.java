a502dfc1af5940d8ded898e8bdd0d9be961c3949
rnjswngus275 <rnjswngus275@gmail.com>
2021-03-16 17:30:59 +0900

    #17 네비게이션바 생성

--- app/src/main/java/com/example/stop_and_flight/MainActivity.java
+++ app/src/main/java/com/example/stop_and_flight/MainActivity.java
@@ -1,23 +1,103 @@
 package com.example.stop_and_flight;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import androidx.appcompat.widget.Toolbar;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 import me.relex.circleindicator.CircleIndicator;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private DrawerLayout mDrawerLayout;
+    private Context context = this;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_signup_method);
+        setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
+        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon); //뒤로가기 버튼 이미지 지정
+
+        //<div>아이콘 제작자 <a href="https://www.flaticon.com/kr/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/kr/" title="Flaticon">www.flaticon.com</a></div>
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                int id = menuItem.getItemId();
+                String title = menuItem.getTitle().toString();
+
+                if(id == R.id.menu1){       //여기에 메뉴 버튼 클릭했을때 옮길 페이지 연결하시면 됩니다.
+                    Toast.makeText(context, title + ": menu1 성공.", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu2){
+                    Toast.makeText(context, title + ": menu2 성공.", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu3){
+                    Toast.makeText(context, title + ": menu3 성공", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu4){
+                    Toast.makeText(context, title + ": menu4 성공", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu5){
+                    Toast.makeText(context, title + ": menu5 성공", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu6){
+                    Toast.makeText(context, title + ": menu6 성공", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu7){
+                    Toast.makeText(context, title + ": menu7 성공", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu8){
+                    Toast.makeText(context, title + ": menu8 성공", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu9){
+                    Toast.makeText(context, title + ": menu9 성공", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu10){
+                    Toast.makeText(context, title + ": menu10 성공", Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
