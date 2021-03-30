817ec5231d09340e8cc3c41b04099796e0cd3f06
rnjswngus275 <38181228+rnjswngus275@users.noreply.github.com>
2021-03-30 16:01:21 +0900

    Revert "#27 비상탈출 팝업 이미지 추가"

--- app/src/main/java/com/example/stop_and_flight/Fragment_flight1.java
+++ app/src/main/java/com/example/stop_and_flight/Fragment_flight1.java
@@ -31,111 +31,8 @@ public class Fragment_flight1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_flight1, container, false);
-
-        Button emergencybutton = v.findViewById(R.id.button_emergency);
-        Button appaccessbutton = v.findViewById(R.id.button_app);
-
-        List<PackageInfo> packlist = new List<PackageInfo>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(@Nullable Object o) {
-                return false;
-            }
-
-            @NonNull
-            @Override
-            public Iterator<PackageInfo> iterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @NonNull
-            @Override
-            public <T> T[] toArray(@NonNull T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(PackageInfo packageInfo) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(@Nullable Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(@NonNull Collection<? extends PackageInfo> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, @NonNull Collection<? extends PackageInfo> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public PackageInfo get(int index) {
-                return null;
-            }
-
-            @Override
-            public PackageInfo set(int index, PackageInfo element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, PackageInfo element) {
-
-            }
-
-            @Override
-            public PackageInfo remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(@Nullable Object o) {
-                return 0;
-            }
         // Inflate the layout for this fragment
-        return v;
+        return inflater.inflate(R.layout.fragment_flight1, container, false);
     }
 
 
@@ -152,23 +49,6 @@ public class Fragment_flight1 extends Fragment {
 
             }
         };
-        emergencybutton.setOnClickListener(this);
-        appaccessbutton.setOnClickListener(this);
-        // Inflate the layout for this fragment
-        return v;
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button_emergency:
-                ShowEmergencyMessage(v);
-                break;
-            case R.id.button_app:
-                ShowAccessApplist(v);
-                break;
-        }
-
     }
 
     private String getTime(){
@@ -203,44 +83,7 @@ public class Fragment_flight1 extends Fragment {
 
     }
 
-    private void ShowEmergencyMessage(View v) {
-        AlertDialog.Builder embuilder = new AlertDialog.Builder(getActivity());
-        embuilder.setTitle("손님!! 비상 탈출 하시겠습니까?");
-        LayoutInflater factory = LayoutInflater.from(getActivity());
-        final View view = factory.inflate(R.layout.emergency_dialog, null);
-        embuilder.setView(view);
-        embuilder.setPositiveButton("살고싶어요..", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getContext(), "끔", Toast.LENGTH_SHORT).show();
-            }
-        });
-        embuilder.setNegativeButton("조금 더 해볼래요", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getContext(), "안 끔", Toast.LENGTH_SHORT).show();
-            }
-        });
-        embuilder.show();
-    }
-
-
-
-
-    private void installedApplist(List<String> applist) {
-        List<PackageInfo> packList = getActivity().getPackageManager().getInstalledPackages(0);
-        PackageInfo packInfo = null;
-        for (int i=0; i < packList.size(); i++)
-        {
-            packInfo = packList.get(i);
-            if ((packInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0)
-            {
-                applist.add(packInfo.packageName);
-            }
-        }
-    }
-
-      @Override
+    @Override
     public void onDestroy() {
         super.onDestroy();
         try{
@@ -248,4 +91,6 @@ public class Fragment_flight1 extends Fragment {
         } catch (Exception e) {}
         countDownTimer=null;
     }
+
+
 }
