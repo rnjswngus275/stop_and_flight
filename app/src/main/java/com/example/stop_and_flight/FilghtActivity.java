fecb02775e64e592bc72365642e76489c085b4fd
kimaram <rladkfka0902@gmail.com>
2021-03-30 16:28:45 +0900

    회원가입,로그인완료,마이페이지 작업중

---
+++ app/src/main/java/com/example/stop_and_flight/FilghtActivity.java
@@ -0,0 +1,35 @@
+package com.example.stop_and_flight;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import me.relex.circleindicator.CircleIndicator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FilghtActivity extends AppCompatActivity {
+
+    private CircleIndicator mIndicator;
+    AppGuideAdapter adapter = new AppGuideAdapter(getSupportFragmentManager());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filght);
+
+        ViewPager viewPager = findViewById(R.id.pager_filght);
+        setupViewPager(viewPager);
+        mIndicator=(CircleIndicator)findViewById(R.id.indicator_flight);
+        mIndicator.setViewPager(viewPager);
+
+    }
+
+    public void setupViewPager(ViewPager viewPager) {
+        adapter.addFragment(new Fragment_flight1(), "1");
+        adapter.addFragment(new Fragment_flight2(), "2");
+        viewPager.setAdapter(adapter);
+    }
+
+
+}
